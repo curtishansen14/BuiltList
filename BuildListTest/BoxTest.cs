4604ea3b9a4f69ab35de067a523dc24281d34144
Curtis Hansen <curtishansen14@gmail.com>
2018-01-11 14:48:08 -0600

    intial commit

---
+++ BuildListTest/BoxTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BuildListTest
+{
+    [TestClass]
+    public class BoxTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
