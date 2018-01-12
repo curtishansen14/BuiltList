0fe8d93b47a6016ca7e9617e055741c5284e8f7b
Curtis Hansen <curtishansen14@gmail.com>
2018-01-12 10:16:36 -0600

    add char, add decimal

--- CustomListTests/CustomListTests.cs
+++ CustomListTests/CustomListTests.cs
@@ -24,20 +24,21 @@ namespace CustomListTests
             //Assert
             Assert.AreSame(expected, list[0]);
         }
+        //[TestMethod]
+        //public void Add_ManyInts_Add10Ints()
+        //{
 
-        public void Add_ManyInts_Add10Ints()
-        {
+        //    //Arrange
+        //    CustomList<int> list = new CustomList<int>();
+        //    int expected = 7;
+        //    //Act
+        //    list.Add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
-            //Arrange
-            CustomList<int> list = new CustomList<int>();
-            int expected = 7;
-            //Act
-            list.Add(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        //    //Assert
+        //    Assert.AreSame(expected, list[6]);
+        //}
 
-            //Assert
-            Assert.AreSame(expected, list[6]);
-        }
-
+        [TestMethod]
         public void Add_OneString_AddString()
         {
 
@@ -50,7 +51,18 @@ namespace CustomListTests
             Assert.AreSame(expected, list[0]);
         }
 
+        //[TestMethod]
+        //public void Add_MultipleString_AddStrings()
+        //{
+        //    //Arrange
+        //    CustomList<string> list = new CustomList<string>();
+        //    string expected = "dignity";
 
+        //    //Act
+        //    list.Add("Two", "households", "both", "alike", "in", "dignity");
+        //    //Assert
+        //    Assert.AreSame(expected, list[5]);
+        //}
 
     }
 }
