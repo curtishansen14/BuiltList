f29739c1a48bf2103906a27085e349dfd8991ed1
Curtis Hansen <curtishansen14@gmail.com>
2018-01-12 15:39:32 -0600

    built constructor

--- BuildList/CustomList.cs
+++ BuildList/CustomList.cs
@@ -8,22 +8,43 @@ namespace BuildList
 {
     public class CustomList<T>
     {
-        public int count = 0;
-        //Methods go here ie toString, Add, Remove, etc
-        public T[] arr = new T[5];
+        //member variables
+        private int capacity = 5;
+        private int count = 0;
+        public T[] arr;
+        //constructor
+        public CustomList()
+        {
+            arr = new T[capacity];
+        }
 
+        //indexer
         public T this[int i]
         {
             get { return arr[i]; }
             set { arr[i] = value; }
         }
 
-       public T Add(T Input)
+        //member methods 
+
+
+        public void Add(T Input)
         {
 
-            count++; 
-            return Input;
+            count++;
+            if (count / capacity >= .5)
+            {
+                capacity = capacity * 2;
+            }
+
+             
+            
         }
+
+
+
+
+        //getters and setters 
 
         public int Count
         {
